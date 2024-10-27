790bc02d906bdcad3a69c9bfce566f4d80a188a3
Sune Rønne <SURN@nykredit.dk>
2024-10-27 20:29:03 +0100

    Alrighty right

---
+++ Malarkey.Service/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿namespace Malarkey.Service;
+
+public static class DependencyInjection
+{
+    public static WebApplicationBuilder AddConfiguration(this WebApplicationBuilder builder)
+    {
+        builder.Configuration.AddJsonFile("apsettings.json");
+        builder.Configuration.AddJsonFile("apssettings.local.json", optional: true);
+        return builder;
+    }
+
+
+}
--- resources/certificate/generate-certificate.bat
+++ resources/certificate/generate-certificate.bat
@@ -1,1 +1,2 @@
 openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout malarkey.key -out malarkey.crt -config openssl.conf -extensions v3_req
+openssl pkcs12 -export -out malarkey.pfx -inkey malarkey.key -in malarkey.crt -password pass:%PSWD%
