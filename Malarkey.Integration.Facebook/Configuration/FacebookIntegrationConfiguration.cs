514d16e697905953f0adf204f4f6ce9229855232
Sune Rønne <SURN@nykredit.dk>
2024-10-30 13:12:39 +0100

    Ya ya

---
+++ Malarkey.Integration.Facebook/Configuration/FacebookIntegrationConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malarkey.Integration.Facebook.Configuration;
+public class FacebookIntegrationConfiguration
+{
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+    public string CallbackPath { get; set; }
+
+
+}
