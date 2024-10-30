57a069e7d03ef2aaff8aec084ac74fdc752502b5
Sune Rønne <SURN@nykredit.dk>
2024-10-30 22:36:37 +0100

    Alrighty!

--- Malarkey.UI/Pages/Authenticate/AuthenticatePage.razor.cs
+++ Malarkey.UI/Pages/Authenticate/AuthenticatePage.razor.cs
@@ -1,7 +1,27 @@
-﻿namespace Malarkey.UI.Pages.Authenticate;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Malarkey.UI.Pages.Authenticate;
 
 public partial class AuthenticatePage
 {
+    [Inject]
+    public NavigationManager NavManager { get; set; }
 
+    public void OnMicrosoftClick(MouseEventArgs e)
+    {
+        NavManager.NavigateTo("authenticate-microsoft");
+        NavManager.Refresh(forceReload: true);
+    }
+
+    public void OnGoogleClick(MouseEventArgs e)
+    {
+        NavManager.NavigateTo("authenticate-microsoft");
+    }
+
+    public void OnFacebookClick(MouseEventArgs e)
+    {
+        NavManager.NavigateTo("authenticate-microsoft");
+    }
 
 }
