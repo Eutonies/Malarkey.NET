a93d4af6f4a51799bfb45b73a986dcb54f1dc3fb
Sune Rønne <SURN@nykredit.dk>
2024-10-30 21:35:00 +0100

    Graphics

--- Malarkey.UI/Pages/Authenticate/AuthenticatePage.razor.cs
+++ Malarkey.UI/Pages/Authenticate/AuthenticatePage.razor.cs
@@ -1,37 +1,7 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components;
-using Malarkey.Application.ProfileImport;
-using Malarkey.Domain.ProfileImport;
-
-namespace Malarkey.UI.Pages.Authenticate;
+﻿namespace Malarkey.UI.Pages.Authenticate;
 
 public partial class AuthenticatePage
 {
-    [Inject]
-    public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
-
-    [Inject]
-    public IProfileImporter<MicrosoftImportProfile> ProfileImporter { get; set; }
-
-
-    private string? _profilePhoto;
-    private IReadOnlyCollection<ImportImage> _images = [];
-
-    protected override async Task OnInitializedAsync()
-    {
-        await base.OnInitializedAsync();
-        var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
-        if (authState != null && authState.User?.Identity != null && authState.User.Identity.IsAuthenticated)
-        {
-            var user = authState.User;
-            var identity = user.Identity;
-            var profile = await ProfileImporter.LoadForImport();
-            if(profile?.Images != null)
-               _images = profile.Images.ToList();
-            await InvokeAsync(StateHasChanged);
-        }
-
-    }
 
 
 }
