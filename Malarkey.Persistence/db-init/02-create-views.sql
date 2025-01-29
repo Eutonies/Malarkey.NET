5317aeb43b8d9fdbd33a267cfda2964fe8eb7b01
Sune Rønne <surn@nykredit.dk>
2025-01-29 23:36:08 +0100

    Los permiciones

--- Malarkey.Persistence/db-init/02-create-views.sql
+++ Malarkey.Persistence/db-init/02-create-views.sql
@@ -18,28 +18,5 @@ with recursive absorbs as (
 select 
 absorbs.profile_id,
 absorbs.absorbee
-from absorbs
+from absorbs;
 
-;
-
-
-create view profile_absorber as 
-with recursive absorbs as (
-  select 
-    profile_id, 
-    absorbed_by as absorber
-  from profile
-  where absorbed_by is not null
-  union all 
-  select 
-    abso.profile_id, 
-    prof.absorbed_by as absorber
-  from absorbs abso
-  inner join profile prof
-  on prof.profile_id = abso.profile_id
-)
-
-select
-  profile_id,
-  absorber
-from absorbs
